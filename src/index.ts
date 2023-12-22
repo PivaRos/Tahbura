b43bea19579e0f1a22861d2acc8f5d8cf87d8c6b
Daniel Gurbin <daniel.g@msapps.mobi>
2023-12-22 18:37:34 +0200

    add queue and Map

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,24 @@ import { router as ApiRouter } from "./routes/api";
 import { router as StationRouter } from "./routes/station";
 import { router as LiveRouter } from "./routes/live";
 import cron from "node-cron";
+import Queue from "./modules/Queue";
+import { ILineTracking } from "./models/tacking";
 
 const app = express();
 
 app.use(express.json());
 
+const Tracking = new Map<string, ILineTracking>();
+const TrackingQueue = new Queue<number>();
+
 cron.schedule("* * * * *", () => {
   //run live updates
+  if (!TrackingQueue.isEmpty()) {
+    TrackingQueue.queue.forEach((trackingItem) => {
+      //TODO:run tracking live location with gov server
+      //TODO:update the Tracking information
+    });
+  }
 });
 
 app.use("/api", ApiRouter);
